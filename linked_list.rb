88d1cc45b900075b1888bcef7d3fabeaef366401
Ilya Krasnov <ilya.krasnov@gmail.com>
2014-08-14 14:37:24 +0200

    Finished reverse linked list

--- linked_list.rb
+++ linked_list.rb
@@ -11,20 +11,24 @@ ll2.payload = "Another payload"
 ll3 = LinkedList.new
 ll3.payload = "3rd one"
 
+ll4 = LinkedList.new
+ll4.payload = "Number 4"
+
+ll5 = LinkedList.new
+ll5.payload = "High five"
+
+ll6 = LinkedList.new
+ll6.payload = "The sixth sense"
+
+
 ll1.next = ll2
 ll2.next = ll3
-
-def iterate_recursive(l)
-		if l.nil? 
-			return
-		else 
-			puts l.payload
-			iterate_recursive(l.next)
-		end
-end
+ll3.next = ll4
+ll4.next = ll5
+ll5.next = ll6
 
 # iterate_recursive(ll1)
-def iterate_functional(l)
+def show_functional(l)
 	node = l
 	puts l.payload
 	while !node.next.nil?
@@ -33,16 +37,59 @@ def iterate_functional(l)
 	end
 end
 
+# Try putting every node in an array
+def list_to_array(l)
+	node = l
+	arr = []
+	arr << l
+	while !node.next.nil?
+		node = node.next
+		arr << node
+	end
+	return arr
+end
 
-iterate_functional(ll1)
+# Re-iterate the array and reset next links
+def array_to_list(arr)
+	arr.each_index do |list|
+		if list != 0
+			arr[list].next = arr[list-1]
+		else
+			arr[list].next = nil
+		end
+	end
+	return arr[arr.length-1]
+end
+
+# Combine two functions in one and show the result
+def reverse(l) 
+	show_functional(array_to_list(list_to_array(l)))
+end
+
+print "Original list:\n\n"
+show_functional(ll1)
+
+puts "\n"
+
+print "Reversed list:\n\n"
+reverse(ll1)
+puts "\n"
 
 
----- HOMEWOK ----
+# def iterate_recursive(l)
+# 		if l.nil? 
+# 			return
+# 		else 
+# 			puts l.payload
+# 			iterate_recursive(l.next)
+# 		end
+# end
+
+
+# ---- HOMEWOK ----
 # 1st
 
-def reverse(l)
-	# 
-end
+
 
 
 # iterate_functional(reverse(ll1))
